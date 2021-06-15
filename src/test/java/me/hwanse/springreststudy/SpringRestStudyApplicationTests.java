03aec9cab271045935bc563fbc4c1a9573642714
hwanse <khsb2012@gmail.com>
2021-06-15 18:20:03 +0900

    initial commit.

---
+++ src/test/java/me/hwanse/springreststudy/SpringRestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwanse.springreststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
