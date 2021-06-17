ab103408ab0bc3886784293d96358cf846755276
hwanse <khsb2012@gmail.com>
2021-06-18 00:40:37 +0900

    test: 인덱스 API Rest Docs 추가

--- src/test/java/me/hwanse/springreststudy/index/IndexControllerTest.java
+++ src/test/java/me/hwanse/springreststudy/index/IndexControllerTest.java
@@ -1,5 +1,10 @@
 package me.hwanse.springreststudy.index;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -35,8 +40,16 @@ public class IndexControllerTest {
   public void index() throws Exception {
     mockMvc.perform(get("/api/"))
            .andExpect(status().isOk())
-           .andExpect(jsonPath("_links.events").exists());
-
+           .andExpect(jsonPath("_links.events").exists())
+           .andDo(document("index",
+                           links(
+                             linkWithRel("events").description("link to events query")
+                           ),
+                           responseFields(
+                             fieldWithPath("_links.events.*").ignored()
+                           )
+                  ))
+    ;
   }
 
 }
