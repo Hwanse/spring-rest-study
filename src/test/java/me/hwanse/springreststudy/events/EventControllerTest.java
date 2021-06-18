a5aa5260dd01cb90e4ab73dfa26fc36dc7718ed6
hwanse <khsb2012@gmail.com>
2021-06-18 20:19:53 +0900

    refactor: controller layer test 중복 코드를 BaseControllerTest 파일 하나로 정리

--- src/test/java/me/hwanse/springreststudy/events/EventControllerTest.java
+++ src/test/java/me/hwanse/springreststudy/events/EventControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.li
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -21,37 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
-import me.hwanse.springreststudy.common.RestDocsConfiguration;
+import me.hwanse.springreststudy.common.BaseControllerTest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTest {
-
-  @Autowired
-  MockMvc mockMvc;
-
-  @Autowired
-  ObjectMapper objectMapper;
+public class EventControllerTest extends BaseControllerTest {
 
   @Autowired
   EventRepository eventRepository;
--- src/test/java/me/hwanse/springreststudy/index/IndexControllerTest.java
+++ src/test/java/me/hwanse/springreststudy/index/IndexControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import me.hwanse.springreststudy.common.BaseControllerTest;
 import me.hwanse.springreststudy.common.RestDocsConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,15 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
  * 모든 홈페이지에는 index 페이지 즉 첫 home 화면이 존재하듯이
  * Rest API도 진입점이라는 것을 알리는 Index API가 필요하다
  */
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class IndexControllerTest {
-
-  @Autowired
-  MockMvc mockMvc;
+public class IndexControllerTest extends BaseControllerTest {
 
   @Test
   @DisplayName("index API 테스트")
