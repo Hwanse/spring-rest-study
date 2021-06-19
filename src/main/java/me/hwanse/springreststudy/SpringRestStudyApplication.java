76da20152561562f8f20e714864dbcadd88a491d
hwanse <khsb2012@gmail.com>
2021-06-19 15:28:29 +0900

    feat: security configuration 추가

--- src/main/java/me/hwanse/springreststudy/SpringRestStudyApplication.java
+++ src/main/java/me/hwanse/springreststudy/SpringRestStudyApplication.java
@@ -4,17 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringRestStudyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringRestStudyApplication.class, args);
-  }
-
-  @Bean
-  public ModelMapper modelMapper() {
-    return new ModelMapper();
   }
 
 }
---
+++ src/main/java/me/hwanse/springreststudy/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package me.hwanse.springreststudy.config;
+
+import lombok.RequiredArgsConstructor;
+import me.hwanse.springreststudy.account.AccountService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final AccountService accountService;
+
+  private final PasswordEncoder passwordEncoder;
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new InMemoryTokenStore();
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(accountService)
+        .passwordEncoder(passwordEncoder);
+  }
+
+  /**
+   * HttpSecurity 검증 단계로 넘어가기 전에 Security Filter Chain 을 거치지 않고 싶다면
+   * WebSecurity configure 를 통해 적용할지 말지에 대한 여부를 설정 필요
+   */
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    // Rest Docs 에는 Security 를 적용하지 않는다
+    // Spring Boot 의 정적 리소스에 Security 를 적용하지 않는다.
+    web.ignoring()
+       .mvcMatchers("/docs/index.html")
+       .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .mvcMatchers("/docs/index.html").anonymous()
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).anonymous()
+    ;
+  }
+}
